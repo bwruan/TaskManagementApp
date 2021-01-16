baf18a5a1343add5c4a3bd9ff78956893f92ea27
Becky <becky.ruan524@gmail.com>
2021-01-16 15:55:38 -0800

    entities created

---
+++ ProjectManagement/Project.Infrastructure/Repositories/Entities/Project.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Project.Infrastructure.Repositories.Entities
+{
+    public partial class Project
+    {
+        public Project()
+        {
+            UserToProjects = new HashSet<UserToProject>();
+        }
+
+        public long ProjectId { get; set; }
+        public string ProjectName { get; set; }
+        public string ProjectDescription { get; set; }
+        public long OwnerAccountId { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+
+        public virtual ICollection<UserToProject> UserToProjects { get; set; }
+    }
+}
