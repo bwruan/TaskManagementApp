8a9c86f8561e5eae0c27e4fd1c25d7c9a1545531
Jason Ruan <jason.ruan@lytx.com>
2021-01-20 13:42:53 -0800

    fix comments again

--- ProjectManagement/Project.Infrastructure/Repository/UserToProjectRepository.cs
+++ ProjectManagement/Project.Infrastructure/Repository/UserToProjectRepository.cs
@@ -11,6 +11,9 @@ namespace Project.Infrastructure.Repository
 {
     public class UserToProjectRepository : IUserToProjectRepository
     {
+
+        //you ignored my comment about LINQ's include.
+        //I told you that unless you do Include, the "Project" property of UserToProjects class is NULL.
         public async Task<List<Entities.Project>> GetProjectsByAccountId(long accountId)
         {
             using (var context = new TaskManagementContext())
@@ -19,6 +22,8 @@ namespace Project.Infrastructure.Repository
 
                 var projectList = new List<Entities.Project>();
 
+                //Mappers do not belong in infrastructure. Where is your mapper in User Management service?
+                //We dont do anything is repo asides retrieve things from DB.
                 foreach (var dbProj in project) 
                 {
                     projectList.Add(UserToProjectMapper.UserToProjectToProject(dbProj));
@@ -28,6 +33,9 @@ namespace Project.Infrastructure.Repository
             }
         }
 
+        //Is this even correct?
+        //Think, how many accounts/workers can be assigned to a project? 
+        //Also, you dont need to do the second statement! Think about how to do it with just one LIQN statement.
         public async Task<long> GetAccountByProjectId(long projectId)
         {
             using(var context = new TaskManagementContext())
